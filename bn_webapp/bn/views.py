599e8c3b7a595c7c01e4bad50d45ce50d3e20391
Shabeeb Hasan <shabeebhasan@hotmail.com>
2018-05-06 17:22:18 -0700

    changes

--- bn_webapp/bn/views.py
+++ bn_webapp/bn/views.py
@@ -3,4 +3,14 @@ from django.http import HttpResponse
 from bn.bn_model import get_users_depression_rate
 
 def index(request):
-    return HttpResponse(get_users_depression_rate())
+
+    html = '<table style="width:100%; text-align: center;"><tr><th>USER NAME</th><th>DEPRESSION RATE</th></tr>';
+    data = get_users_depression_rate()
+    for d in data:
+        html += "<tr>"
+        html += '<td>{}</td>'.format(d['user_name'])
+        html += '<td>{}</td>'.format(d['depression_rate'])
+        html += "</tr>"
+
+    html += "</table>"
+    return HttpResponse(html)
